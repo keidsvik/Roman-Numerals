4eefbaf06c7c7d346684a96da752300e003b6ccd
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-04-02 11:11:27 -0700

    confirmed specs layout, implemented spec #1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
+function giveNumber(newNumber) {
+  
   
 }
+
+
+$(document).ready(function() {
+  $("form#romanNumeralInput").submit(function(event) {
+    event.preventDefault();
+    var ourNumber = parseInt($("#input").val());
+    giveNumber(ourNumber);
+  });
+});,
+
